16bd194de42a476857f72d6a761dbce452245fea
allen <allen@gmail.com>
2025-01-23 10:59:41 +0800

    update game logic

---
+++ server/src/etc/cfg/battleMap.lua
@@ -0,0 +1,219 @@
+-- sceneId                          int                              ""id
+-- name                             string                           ""
+-- skybox                           string                           ""
+-- cubemap                          string                           ""
+-- length                           int                              ""*1000
+-- width                            int                              ""*1000
+-- signinPos1                       json                             ""1 * 1000
+-- signinScale1                     json                             ""1 * 1000
+-- signinPos2                       json                             ""2 * 1000
+-- signinScale2                     json                             ""2 * 1000
+-- signinPos3                       json                             ""3 * 1000
+-- signinScale3                     json                             ""3 * 1000
+-- signinPos4                       json                             ""4 * 1000
+-- signinScale4                     json                             ""4 * 1000
+-- skillAreaPos                     json                             "" * 1000
+-- skillAreaScale                   json                             "" * 1000
+
+return {
+	[1] = {
+		sceneId = 1,
+		name = "Terrain_Yellow_02",
+		skybox = "Default-Skybox",
+		cubemap = "cubemap",
+		length = 40000,
+		width = 40000,
+		signinPos1 = {
+		},
+		signinScale1 = {
+		},
+		signinPos2 = {
+		},
+		signinScale2 = {
+		},
+		signinPos3 = {
+			[1] = -31450,
+			[2] = 0,
+			[3] = -2300,
+		},
+		signinScale3 = {
+			[1] = 1500,
+			[2] = 1000,
+			[3] = 1410,
+		},
+		signinPos4 = {
+		},
+		signinScale4 = {
+		},
+		skillAreaPos = {
+			[1] = -5160,
+			[2] = 0,
+			[3] = -3280,
+		},
+		skillAreaScale = {
+			[1] = 43500,
+			[2] = 1000,
+			[3] = 40000,
+		},
+	},
+	[2] = {
+		sceneId = 2,
+		name = "Terrain_HighEneray35_Orange",
+		skybox = "Default-Skybox",
+		cubemap = "cube_Yellow",
+		length = 35000,
+		width = 35000,
+		signinPos1 = {
+		},
+		signinScale1 = {
+		},
+		signinPos2 = {
+		},
+		signinScale2 = {
+		},
+		signinPos3 = {
+			[1] = -26000,
+			[2] = 0,
+			[3] = -7200,
+		},
+		signinScale3 = {
+			[1] = 1000,
+			[2] = 1000,
+			[3] = 1070,
+		},
+		signinPos4 = {
+		},
+		signinScale4 = {
+		},
+		skillAreaPos = {
+			[1] = -6000,
+			[2] = 0,
+			[3] = -6000,
+		},
+		skillAreaScale = {
+			[1] = 36000,
+			[2] = 1000,
+			[3] = 36000,
+		},
+	},
+	[3] = {
+		sceneId = 3,
+		name = "Terrain_HighEneray30_Green",
+		skybox = "Default-Skybox",
+		cubemap = "cube_Green",
+		length = 30000,
+		width = 30000,
+		signinPos1 = {
+		},
+		signinScale1 = {
+		},
+		signinPos2 = {
+		},
+		signinScale2 = {
+		},
+		signinPos3 = {
+			[1] = -26000,
+			[2] = 0,
+			[3] = -9100,
+		},
+		signinScale3 = {
+			[1] = 1000,
+			[2] = 1000,
+			[3] = 900,
+		},
+		signinPos4 = {
+		},
+		signinScale4 = {
+		},
+		skillAreaPos = {
+			[1] = -8360,
+			[2] = 0,
+			[3] = -8360,
+		},
+		skillAreaScale = {
+			[1] = 31000,
+			[2] = 1000,
+			[3] = 31000,
+		},
+	},
+	[4] = {
+		sceneId = 4,
+		name = "Terrain_Mechanics35_Purple",
+		skybox = "Default-Skybox",
+		cubemap = "cube_Purple",
+		length = 35000,
+		width = 35000,
+		signinPos1 = {
+		},
+		signinScale1 = {
+		},
+		signinPos2 = {
+		},
+		signinScale2 = {
+		},
+		signinPos3 = {
+			[1] = -26000,
+			[2] = 0,
+			[3] = -5200,
+		},
+		signinScale3 = {
+			[1] = 1000,
+			[2] = 1000,
+			[3] = 1130,
+		},
+		signinPos4 = {
+		},
+		signinScale4 = {
+		},
+		skillAreaPos = {
+			[1] = -6000,
+			[2] = 0,
+			[3] = -6000,
+		},
+		skillAreaScale = {
+			[1] = 36000,
+			[2] = 1000,
+			[3] = 36000,
+		},
+	},
+	[5] = {
+		sceneId = 5,
+		name = "Terrain_Mechanics30_Blue",
+		skybox = "Default-Skybox",
+		cubemap = "cube_Blue",
+		length = 30000,
+		width = 30000,
+		signinPos1 = {
+		},
+		signinScale1 = {
+		},
+		signinPos2 = {
+		},
+		signinScale2 = {
+		},
+		signinPos3 = {
+			[1] = -26000,
+			[2] = 0,
+			[3] = -8040,
+		},
+		signinScale3 = {
+			[1] = 1000,
+			[2] = 1000,
+			[3] = 1000,
+		},
+		signinPos4 = {
+		},
+		signinScale4 = {
+		},
+		skillAreaPos = {
+			[1] = -8360,
+			[2] = 0,
+			[3] = -8360,
+		},
+		skillAreaScale = {
+			[1] = 31000,
+			[2] = 1000,
+			[3] = 31000,
+		},
+	},
+}
---
+++ server/src/etc/cfg/board.lua
@@ -0,0 +1,25 @@
+-- cfgId                            int                              ID
+-- imageUrl                         string                           ""
+-- url                              string                           ""
+-- text                             string                           ""
+
+return {
+	[1] = {
+		cfgId = 1,
+		imageUrl = "https://img1.doubanio.com/view/celebrity/raw/public/p20908.jpg",
+		url = "https://www.baidu.com/",
+		text = "gg1",
+	},
+	[2] = {
+		cfgId = 2,
+		imageUrl = "https://img2.doubanio.com/view/celebrity/raw/public/p1518431956.11.jpg",
+		url = "https://www.baidu.com/",
+		text = "gg2",
+	},
+	[3] = {
+		cfgId = 3,
+		imageUrl = "https://img1.doubanio.com/view/personage/raw/public/cb5af1aceb3ebe0ca60c9e5d39acd749.jpg",
+		url = "https://www.baidu.com/",
+		text = "gg3",
+	},
+}
