25e87e50293518d14d0c85ea1ddce0ab2cccbb23
allen <allen@gmail.com>
2025-02-24 15:02:22 +0800

    update game logic

---
+++ server/src/etc/cfg/taskActivation.lua
@@ -0,0 +1,31 @@
+-- id                               int                              
+-- activation                       int                              ""
+-- reward                           int                              ""id
+-- icon                             string                           ""
+
+return {
+	[1] = {
+		id = 1,
+		activation = 100,
+		reward = 1,
+		icon = "box 01_icon",
+	},
+	[2] = {
+		id = 2,
+		activation = 200,
+		reward = 2,
+		icon = "box 02_icon",
+	},
+	[3] = {
+		id = 3,
+		activation = 300,
+		reward = 3,
+		icon = "box 03_icon",
+	},
+	[4] = {
+		id = 4,
+		activation = 400,
+		reward = 4,
+		icon = "box 04_icon",
+	},
+}
---
+++ server/src/etc/cfg/textAlert.lua
@@ -0,0 +1,41 @@
+-- name                             string                           ID
+-- textAlert                        string                           ""
+
+return {
+	["alertInstitute"] = {
+		name = "alertInstitute",
+		textAlert = "RESEARCH",
+	},
+	["bgText_CENTRA1"] = {
+		name = "bgText_CENTRA1",
+		textAlert = "race_Centra1",
+	},
+	["bgText_CENTRA2"] = {
+		name = "bgText_CENTRA2",
+		textAlert = "race_Centra2",
+	},
+	["bgText_ENDARI1"] = {
+		name = "bgText_ENDARI1",
+		textAlert = "race_Endari1",
+	},
+	["bgText_ENDARI2"] = {
+		name = "bgText_ENDARI2",
+		textAlert = "race_Endari2",
+	},
+	["bgText_SCOURGE1"] = {
+		name = "bgText_SCOURGE1",
+		textAlert = "race_Scourge1",
+	},
+	["bgText_SCOURGE2"] = {
+		name = "bgText_SCOURGE2",
+		textAlert = "race_Scourge2",
+	},
+	["bgText_TALUS1"] = {
+		name = "bgText_TALUS1",
+		textAlert = "race_Talus1",
+	},
+	["bgText_TALUS2"] = {
+		name = "bgText_TALUS2",
+		textAlert = "race_Talus2",
+	},
+}
